12c9444d35caaf5fa3e7bdd0ac7f33d3d3049bec
AndreAS200320 <129907649+AndreAS200320@users.noreply.github.com>
2024-02-06 22:18:28 -0300

    commit

---
+++ Controllers/PessoaController.cs
@@ -0,0 +1,37 @@
+﻿using APImodelo.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APImodelo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        [HttpGet("GetPessoa")]
+        public Pessoa GetPessoa() 
+        { 
+            Pessoa VarPessoa = new Pessoa();
+
+            VarPessoa.Id = 1;
+            VarPessoa.Nome = "Alex";
+            VarPessoa.NumeroDeCelular = "(18)999999999";
+            
+
+            return VarPessoa;
+        }
+
+        [HttpGet("GetPessoas")]
+
+        public List<Pessoa> GetPessoas()
+        {
+            
+
+            var VPessoa = new Pessoa();
+
+
+
+            return VPessoa.Dados();
+        }
+    }
+}
---
+++ Model/Pessoa.cs
@@ -0,0 +1,42 @@
+﻿namespace APImodelo.Model
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string NumeroDeCelular { get; set; }
+
+        public DateOnly DataNascimento { get; set; }
+
+        //metodo contendo os dados
+
+        public List<Pessoa> Dados()
+        {
+            var ListaPessoas = new List<Pessoa>();
+
+            var VPessoa = new Pessoa();
+
+            VPessoa.Id = 1;
+            VPessoa.Nome = "Muriel";
+            VPessoa.NumeroDeCelular = "(18)999999999";
+
+            var VPessoa2 = new Pessoa();
+
+            VPessoa2.Id = 2;
+            VPessoa2.Nome = "Muller";
+            VPessoa2.NumeroDeCelular = "(18)999999999";
+
+            var VPessoa3 = new Pessoa();
+
+            VPessoa3.Id = 3;
+            VPessoa3.Nome = "Atasaki";
+            VPessoa3.NumeroDeCelular = "(18)999999999";
+
+            ListaPessoas.Add(VPessoa); ListaPessoas.Add(VPessoa2); ListaPessoas.Add(VPessoa3);
+
+            return ListaPessoas;
+        }
+    }
+}
